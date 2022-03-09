24a78c2169eb1dbe37a50045c6088a0091147ee5
Elisabeth <elisabeth.lisha.ye@gmail.com>
2022-03-09 11:32:11 +0100

    Compte/eleve prof(non fini)

--- src/main/java/model/Prof.java
+++ src/main/java/model/Prof.java
@@ -4,11 +4,5 @@ import java.time.LocalDate;
 
 public class Prof  extends Compte{
 
-	public Prof (String nom, String prenom, String login, String password, LocalDate naissance, double solde) {
-		super(nom, prenom, login, password, naissance, solde);
 
-		@Override
-		public String toString() {
-			return "Professeur [Id= "+ id +" Nom= "+ nom+" Prenom= "+ prenom+ " Login= "+ login+" Password= "+ password+ " naissance= "+ naissance+ "Solde= "+ solde+ "]";
-		}
 	}
