1fcf7119b296716870c837ff2e461256d36653ef
Elisabeth <elisabeth.lisha.ye@gmail.com>
2022-03-09 11:20:53 +0100

    Attribut Eleves+Prof

--- src/main/java/model/Eleve.java
+++ src/main/java/model/Eleve.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Eleve  extends Compte{
 
+	private Maisons maison;
+
+	public Eleve(String nom, String prenom, String login, String password, LocalDate naissance, double solde, Maisons maison) {
+		super(nom, prenom, login, password, naissance, solde);
+		this.maison = maison;
+	}
+
+	public Maisons getMaison() {
+		return maison;
+	}
+
+	public void setMaison(Maisons maison) {
+		this.maison = maison;
+	}
+
+	@Override
+	public String toString() {
+		return "Elève [Id= "+ id +" Nom= "+ nom+" Prenom= "+ prenom+ " Login= "+ login+" Password= "+ password+ " naissance= "+ naissance+ "Solde= "+ solde+ " maison=" + maison + "]";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 }
+
+
--- src/main/java/model/Prof.java
+++ src/main/java/model/Prof.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Prof  extends Compte{
 
-}
+	public Prof (String nom, String prenom, String login, String password, LocalDate naissance, double solde) {
+		super(nom, prenom, login, password, naissance, solde);
+
+		@Override
+		public String toString() {
+			return "Professeur [Id= "+ id +" Nom= "+ nom+" Prenom= "+ prenom+ " Login= "+ login+" Password= "+ password+ " naissance= "+ naissance+ "Solde= "+ solde+ "]";
+		}
+	}
