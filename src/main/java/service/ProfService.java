7bc201c458b7213a2cc7ef7ff056425a2affe7ad
MatthieuPinel <mattpinel23.mp@gmail.com>
2022-03-23 17:27:27 +0100

    partie prof ok

---
+++ src/main/java/service/ProfService.java
@@ -0,0 +1,86 @@
+package service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import centerpark.repositories.ReservationRepository;
+import compte.Eleve;
+import compte.Prof;
+import exception.ProfException;
+import repositories.ProfRepository;
+import util.Context;
+
+@Service
+public class ProfService {
+
+	@Autowired
+	private ProfRepository profRepository;
+	
+	public Eleve insert(Eleve el) {
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		em.persist(el);
+		em.getTransaction().commit();
+		em.close();
+		return null;
+		
+	}	
+	public void create(Prof pf) {
+		if (pf.getId() != null) {
+			throw new ProfException("l'id ne doit pas etre defini");
+		}
+		if (pf.getLogin() == null || pf.getLogin().isEmpty()) {
+			throw new ProfException("Login manquant");
+		}
+		if (pf.getPassword() == null || pf.getLogin().isEmpty()) {
+			throw new ProfException("Password manquant");
+		}
+		profRepository.save(pf);
+	}
+
+	public void update(Prof pf) {
+		if (pf.getId() == null) {
+			throw new ProfException("le numero doit etre defini");
+		}
+		if (pf.getLogin() == null ||pf.getLogin().isEmpty()) {
+			throw new ProfException("Login manquant");
+		}
+		profRepository.save(pf);
+	}
+
+	public List<Prof> getAll() {
+		return profRepository.findAll();
+	}
+
+	public Prof getById(Integer id) {
+		return profRepository.findById(id).orElseThrow(() -> {
+			throw new ProfException("id inconnu");
+		});
+	}
+
+	/*public Prof getByNumeroWithReservation(Integer id) {
+		return ProfRepository.findByNumeroWithReservations(numero).orElseThrow(() -> {
+			throw new ClientException("numero inconnu");
+		});
+	}*/
+
+	public void delete(Prof pf) {
+//		Client clientEnBaseAvecReservations = getByNumeroWithReservation(c.getNumero());
+//		reservationRepository.deleteAll(clientEnBaseAvecReservations.getReservations());
+//		clientRepository.delete(clientEnBaseAvecReservations);
+
+		Prof profEnBase = getById(pf.getId());
+		//profRepository.deleteByProf(profEnBase);
+		profRepository.delete(profEnBase);
+	}
+
+	public void deleteByNumero(Integer id) {
+		Prof prof = new Prof();
+		prof.setId(id);
+		delete(prof);
+	}
+}
